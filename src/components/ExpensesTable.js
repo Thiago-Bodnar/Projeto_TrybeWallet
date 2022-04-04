93c94bc7647c595933956a6f807200278712ed8c
Thiago <bodnarthiago@gmail.com>
2022-04-04 17:01:54 -0300

    feat: Requisito 8 feito. Adiciona de forma dinamica na tela as despensas adicionadas.

--- src/components/ExpensesTable.js
+++ src/components/ExpensesTable.js
@@ -1,21 +1,71 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class ExpensesTable extends Component {
+  convertData = () => {
+    const { expenses } = this.props;
+    const convertedArray = expenses.map((expense) => {
+      const {
+        id,
+        description,
+        tag,
+        method,
+        value,
+        currency,
+      } = expense;
+      const { ask, name } = Object.values(expense.exchangeRates)
+        .find((exchange) => exchange.code === currency);
+      const fixedValue = (ask * value).toFixed(2);
+      return {
+        id,
+        description,
+        tag,
+        method,
+        ask: (+ask).toFixed(2),
+        value: (+value).toFixed(2),
+        currency: name,
+        fixedValue,
+        defaultCurrency: 'Real',
+      };
+    });
+    return convertedArray;
+  };
+
   render() {
+    const convertedExpenses = this.convertData();
     return (
       <table>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+
+          {
+            convertedExpenses.map((expense) => (
+              <tr key={ expense.id }>
+                <td>{ expense.description }</td>
+                <td>{ expense.tag }</td>
+                <td>{ expense.method }</td>
+                <td>{ expense.value }</td>
+                <td>{ expense.currency }</td>
+                <td>{ expense.ask }</td>
+                <td>{ expense.fixedValue }</td>
+                <td>{ expense.defaultCurrency }</td>
+              </tr>
+            ))
+          }
+        </tbody>
       </table>
     );
   }
@@ -24,5 +74,9 @@ class ExpensesTable extends Component {
 const mapStateToProps = (state) => ({
   expenses: state.wallet.expenses,
 });
+
+ExpensesTable.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.any).isRequired,
+};
 
 export default connect(mapStateToProps, null)(ExpensesTable);
